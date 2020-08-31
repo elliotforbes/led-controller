41eed553ae23d89d2d9acd201e890d76509f8cab
Elliot Forbes <emforce@live.com>
2020-08-31 20:00:29 +0100

    Updates

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
-from astral.sun import sun 
 from astral import LocationInfo
-from datetime import datetime
+from astral.sun import sun
+from datetime import datetime, timezone
+import time
 
 def setup_astral():
     city = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
@@ -12,22 +13,24 @@ def main():
 
     city = setup_astral()
     s = sun(city.observer, date=datetime.now())
-    
+
     print(s["sunrise"])
     print(s["sunset"])
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(18,GPIO.OUT)
 
     while True:
-        t = datetime.now()
+        t = datetime.now(timezone.utc)
 
-        if sunrise < t < sunset:
-            GPIO.output(18, GPIO.HIGH)
-        if t < sunrise || t > sunset:
-            GPIO.output(18, GPIO.LOW) 
+        if s["sunrise"] < t < s["sunset"]:
+            print("Time is greater than sunrise but less than sunset")
+            GPIO.output(18,GPIO.LOW)
+        elif t < s["sunrise"] or t > s["sunset"]:
+            GPIO.output(18,GPIO.HIGH)
+
+        time.sleep(60)
 
 if __name__ == "__main__":
-    main
-
+    main()
