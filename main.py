4c03176fe9f1a75e88ba15eba8785a083947020f
Elliot Forbes <emforce@live.com>
2020-09-12 15:00:53 +0100

    Fixed logic bug where the program didn't last multiple days

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ longitude = 0.1278
 def main():
     print("LED Controller Starting...")
 
-    sun = Sun(latitude, longitude)
-
-    sunrise = sun.get_sunrise_time()
-    sunset = sun.get_sunset_time()
-
-    print(sunrise)
-    print(sunset)
-
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(18,GPIO.OUT)
     GPIO.output(18,GPIO.HIGH)
     while True:
+
+        sun = Sun(latitude, longitude)
+        sunrise = sun.get_sunrise_time()
+        sunset = sun.get_sunset_time()
+        print(sunrise)
+        print(sunset)
+
         t = datetime.now(timezone.utc)
-        seven_am = datetime.now(timezone.utc).replace(hour=7,minute=0,second=0,microsecond=0)
+        print(t)
+
+        seven_am = datetime.now(timezone.utc).replace(hour=6,minute=0,second=0,microsecond=0)
         if seven_am < t < sunset:
             print("Time is greater than sunrise but less than sunset")
             GPIO.output(18,GPIO.LOW)
